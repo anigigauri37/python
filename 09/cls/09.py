2207b1e2a05ec5cc4d021e5acc8f382d4b36f9aa
anigigauri37 <anigigauri37@gmail.com>
2024-09-12 19:35:35 +0400

    commit

--- 09/cls/09.py
+++ 09/cls/09.py
@@ -22,3 +22,84 @@ print(books2)
 
 cars = ['merso', 'bmw']
 print(cars[len(cars) -1])
+
+
+#5
+
+cars = ["ferari", "mercedesi", "toiota"]
+cars[2] = "subaro"
+print(len(cars))
+
+
+#6
+
+cars = ["ferari", "mercedesi", "toiota"]
+cars.append("ani")
+print(cars)
+
+#append - დამატება
+
+
+#7 sheqmenit punqcia romelic 1 dan 10 gamoitvlis yvela ricxvs da daamatebs siashi
+
+numbers = []
+
+for i in range(1,10):
+    numbers.append(i)
+
+print(numbers)
+print(len(numbers))
+
+
+#8
+
+start = int(input("enter start number"))
+end = int(input("enter end number"))
+
+numbers = []
+for i in range(start, end):
+    numbers.append(i)
+    print(numbers)
+
+
+#9 max
+
+numbers = [1,2,3,4,5]
+
+print(max(numbers))
+
+
+#10 min
+
+numbers = [5,2,3,10,25]
+
+print(min(numbers))
+
+
+#11 shemovitamos ricxvi da davbewdot am ricxvebis max, min, yvela ricxvis jami
+
+start = int(input("enter start number"))
+end = int(input("enter end number"))
+
+numbers = []
+for i in range(start, end):
+    numbers.append(i)
+
+print(max(numbers))
+print(min(numbers))
+print(sum(numbers))
+
+
+#12
+
+count = int(input("enter numbers"))
+
+numbers = []
+
+for i in range(count):
+    num = int(input("enter number " + str(i + 1 ) + ": "))
+    numbers.append(num)
+    print(numbers)
+
+
+#13
